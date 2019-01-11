d5ad272de2e70e50ce12145826a0b895cc150096
joao <joao182cx@gmail.com>
2019-01-11 13:25:47 -0200

    Criação do construtor de PieceWorker, que por sua vez chama o construtor da sua superclasse Employee.

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,18 @@
 
 public class Employee {
+	
+	private String nome;
+	private String sobrenome;
+	private String cpf;
+	
+	
+	public Employee(String nome, String sobrenome, String cpf) {
+		super();
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.cpf = cpf;
+	}
+	
+	
 
 }
--- src/PieceWorker.java
+++ src/PieceWorker.java
@@ -2,6 +2,11 @@
 public class PieceWorker extends Employee{
 	
 	
+	public PieceWorker(String nome, String sobrenome, String cpf) {
+		super(nome, sobrenome, cpf);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public String toString() {
 		return super.toString();
